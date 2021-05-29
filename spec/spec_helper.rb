55f3cc87ce4923eb26f2880aaf3f5870c505a30c
Alexande B <abobrikovich@gmail.com>
2021-05-29 13:16:07 +0300

    Ruby 3.0 add missing require "fileutils"

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require "bundler/setup"
 require "lapidist/cli/command"
 require "support/console_helper"
+require "fileutils"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
